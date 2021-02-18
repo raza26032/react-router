e0b17cd49848731f5591c09a28ad7b8ffcc2ffdd
HifsaAnsari <hifsa.ansari12@gmail.com>
2021-02-18 14:08:49 +0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Login from "./components/login/login.jsx"
+import Signup from "./components/signup/signup.jsx"
+import Dashboard from "./components/dashboard/dashboard.jsx"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+          <img src={('https://logodownload.org/wp-content/uploads/2018/02/reddit-logo.png')} />
+        <div className="App">
+        <input type="search" id="header-search-bar" name="q" placeholder="Search" value=""></input>
+          <nav className="nav">
+            <Link to="/" className="one">login</Link>
+            <Link to="/signup" className="two">Signup</Link>
+            <Link to="/dashboard" className="three">Dashboard</Link>
+          </nav>
+        </div>
+
+
+        <Switch>
+
+
+          <Route exact={true} path="/">
+            <Login />
+          </Route>
+
+
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+
+
+          <Route path="/signup">
+            <Signup />
+          </Route>
+
+
+
+
+        </Switch>
+      </Router>
     </div>
   );
 }
